8b41c7aa9bc1b7e95e96406981e44ef03977427b
geo.c <geo.c@kakaocorp.com>
2026-02-10 14:31:42 +0900

    ..

--- src/main/java/geo/toy/ToyApplication.java
+++ src/main/java/geo/toy/ToyApplication.java
@@ -2,8 +2,12 @@ package geo.toy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @SpringBootApplication
 @RestController
@@ -15,5 +19,12 @@ public class ToyApplication {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/word")
+    public ResponseEntity<Void> word() {
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create("/word/index.html"))
+                .build();
     }
 }
